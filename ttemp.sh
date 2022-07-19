4774a4714ddc899be43e7bd667411671948f1e4c
Laurence Finston <Laurence.Finston@gmx.de>
2022-07-19 16:56:24 +0200

    Added

---
+++ ttemp.sh
@@ -0,0 +1,5 @@
+#### ttemp.sh
+#### Created by Laurence D. Finston (LDF) Tue 19 Jul 2022 04:54:59 PM CEST
+
+cp -f bibliography.txt ~/www/personal/SRC_CODE/Scale_Layouts
+cp -f bibliography.pdf ~/www/personal/graphics/Scale_Layouts
