70b5e56d3c493e77c471c27334467e79750da9e0
Laurence Finston <lf@laurence.finston@gmx.de>
2024-07-02 17:40:28 +0200

    Added.

---
+++ pure.cxx
@@ -0,0 +1,83 @@
+/* pure.cxx  */
+/* Created by Laurence D. Finston (LDF) Di 2. Jul 17:38:33 CEST 2024 */
+
+/* * (1) Copyright and License. */
+
+/* Copyright 2022 Laurence D. Finston */
+
+/* This file is part of Scale_Layouts, a package for generating scale layouts. */
+
+/* Scale_Layouts is free software; you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 3 of the License, or */
+/* (at your option) any later version.  */
+
+/* Scale_Layouts is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details.  */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with GNU 3DLDF; if not, write to the Free Software */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+/* ("@@" stands for a single at-sign in the following paragraph.) */
+
+/* Please send bug reports to Laurence.Finston@@gmx.de */
+
+/* The author can be contacted at: */
+
+/* Laurence D. Finston                 */
+/* Laurence.Finston@@gmx.de (@@ stands for a single "at" sign.) */
+
+#include <stdlib.h>
+#include <cstdio>
+#include <float.h>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <math.h>
+#include <new>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+stringstream s;
+
+
+// (setq outline-regexp "/\\* [*\f]+")
+
+/* * (1) */
+
+int main(int argc, char *argv[])
+{
+/* ** (2) */
+   
+
+
+   return 0;
+
+}
+
+/* * Emacs-Lisp code for use in indirect buffers when using the          */
+/*   GNU Emacs editor.  The local variable list is not evaluated when an */
+/*   indirect buffer is visited, so it's necessary to evaluate the       */
+/*   following s-expression in order to use the facilities normally      */
+/*   accessed via the local variables list.                              */
+/*   \initials{LDF 2004.02.12}.                                          */
+/*   (progn (cweb-mode) (outline-minor-mode t))                          */
+
+/* * Local variables for Emacs.*/
+/* Local Variables: */
+/* mode:CWEB */
+/* eval:(display-time) */
+/* eval:(read-abbrev-file) */
+/* indent-tabs-mode:nil */
+/* eval:(outline-minor-mode) */
+/* fill-column:80 */
+/* End: */
+
+
